29040546d05f072bb5f5c48885cf46dceb9f3e9f
Ilyar Mnazhdin <ilyarmnzhdn@gmail.com>
2017-04-24 10:44:49 +0300

    Add Firebase, create user on Firebase

--- finalProject/SignUpViewController.swift
+++ finalProject/SignUpViewController.swift
@@ -7,52 +7,90 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUp: UIViewController {
     
-    let plusPhotoButton: UIButton = {
+    lazy var plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
         return button
     }()
     
-    let emailTextField: UITextField = {
+    lazy var emailTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "email..."
         textField.backgroundColor = UIColor(white: 0, alpha: 0.02)
         textField.borderStyle = .roundedRect
         textField.font = UIFont.systemFont(ofSize: 14)
+        textField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return textField
     }()
     
-    let usernameTextField: UITextField = {
+    func handleTextInputChange(){
+        let isFormValid = emailTextField.text?.characters.count ?? 0 > 0 && usernameTextField.text?.characters.count ?? 0 > 0 && passwordTextField.text?.characters.count ?? 0 > 0
+        
+        if isFormValid {
+            signUpButton.isEnabled = true
+            signUpButton.backgroundColor = UIColor.rgb(red: 17, green: 154, blue: 237)
+        } else {
+            signUpButton.isEnabled = false
+            signUpButton.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+        }
+    }
+    
+    lazy var usernameTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "username..."
         textField.backgroundColor = UIColor(white: 0, alpha: 0.02)
         textField.borderStyle = .roundedRect
         textField.font = UIFont.systemFont(ofSize: 14)
+        textField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return textField
     }()
     
-    let passwordTextField: UITextField = {
+    lazy var passwordTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "password..."
         textField.isSecureTextEntry = true
         textField.backgroundColor = UIColor(white: 0, alpha: 0.02)
         textField.borderStyle = .roundedRect
         textField.font = UIFont.systemFont(ofSize: 14)
+        textField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return textField
     }()
     
-    let signUpButton: UIButton = {
+    lazy var signUpButton: UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
         button.layer.cornerRadius = 8
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         button.setTitleColor(.white, for: .normal)
         button.setTitle("Sign Up", for: .normal)
+        
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        button.isEnabled = false
+        
         return button
     }()
+    
+    func handleSignUp(){
+        guard let email = emailTextField.text, !email.isEmpty
+            else { return }
+        guard let username = usernameTextField.text, !username.isEmpty
+            else { return }
+        guard let password = passwordTextField.text, !password.isEmpty
+            else { return }
+        
+        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user: FIRUser?, error: Error?) in
+            if let err = error {
+                print(err.localizedDescription)
+                return
+            }
+            
+            print("Successfully created user", user?.uid ?? "")
+        })
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
