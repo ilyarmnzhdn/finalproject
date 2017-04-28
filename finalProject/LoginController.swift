dbb7ccfe1021e24b64370b6f83b36abb20ee5ee7
Ilyar Mnazhdin <ilyarmnzhdn@gmail.com>
2017-04-28 11:38:28 +0300

    Add lendify logo to LogIn Controller

--- finalProject/LoginController.swift
+++ finalProject/LoginController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class LoginController: UIViewController {
     
+    lazy var logoContainerView: UIView = {
+        let view = UIView()
+        
+        // Lendify logo
+        let logoImageView = UIImageView(image: #imageLiteral(resourceName: "lendify-logo"))
+        logoImageView.contentMode = .scaleAspectFill
+        
+        view.addSubview(logoImageView)
+        logoImageView.anchor(top: nil, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 200, height: 50)
+        // Center the logo in the view.
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        // Light blue color
+        view.backgroundColor = UIColor.rgb(red: 111, green: 208, blue: 243)
+        return view
+    }()
+    
     lazy var signUpButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Don't have an account? Sign Up.", for: .normal)
@@ -22,12 +40,19 @@ class LoginController: UIViewController {
         navigationController?.pushViewController(signUpController, animated: true)
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.rgb(red: 240, green: 250, blue: 251)
+        
+        view.addSubview(logoContainerView)
+        logoContainerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 150)
         
         navigationController?.isNavigationBarHidden = true
         
-        view.backgroundColor = .white
         view.addSubview(signUpButton)
         signUpButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
     }
