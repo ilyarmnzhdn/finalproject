3e05628b89cd0bce693492b619855350364c736e
Ilyar Mnazhdin <ilyarmnzhdn@icloud.com>
2017-05-06 12:34:29 +0300

    feat: Capture photo

---
+++ finalProject/CameraController+Extention.swift
@@ -0,0 +1,9 @@
+//
+//  CameraController+Extention.swift
+//  finalProject
+//
+//  Created by Ильяр Мнаждин on 5/6/17.
+//  Copyright © 2017 Ilyar Mnazhdin. All rights reserved.
+//
+
+import Foundation
